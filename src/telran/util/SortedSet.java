0760ee56a72bba45643a807e407d524357b93d0d
Yaroslav <sewa130391@gmail.com>
2022-09-01 18:35:28 +0300

    HW#22

--- src/telran/util/SortedSet.java
+++ src/telran/util/SortedSet.java
@@ -3,14 +3,30 @@ package telran.util;
 public interface SortedSet<T> extends Set<T> {
 	/**
 	 * 
-	 * @return reference to the last object
+	 * @return reference to the least object
 	 */
 	T first();
+
+	/**
+	 * 
+	 * @return reference to the most object
+	 */
+	T last();
 	
 	/**
 	 * 
-	 * @return reference 
+	 * @param pattern
+	 * @return Returns the least element in this set
+	 *  greater than or equal to the given element,
+	 *   or null if there is no such element.
 	 */
-	 T last();
-	 
+	T ceiling(T pattern) ;
+	/**
+	 * 
+	 * @param pattern
+	 * @return Returns the greatest element in this set
+	 *  less than or equal to the given element,
+	 *   or null if there is no such element.
+	 */
+	T floor(T pattern) ;
 }
